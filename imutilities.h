e625da4bf5d2862f53acc2139558c762ee3e125c
Isac Maximo <88113372+isacmaximo@users.noreply.github.com>
2021-08-22 09:42:11 -0300

    pequenas alterações na organização

--- imutilities.h
+++ imutilities.h
@@ -24,45 +24,45 @@
 
 
 //função que lê arquivos linha por linha
-void ler_arq(char nomearq[100], int tamlinha){   //recebe o nome do arquivo e o tamanho da linha
-    char linha[tamlinha];  //o tamanho da linha é definida 
-    FILE * arq = fopen(nomearq, "r");  //topo arquivo tem um ponteiro que vai ser aberto em modo leitura
-    while(fgets(linha, sizeof(linha), arq) != NULL){ //enquanto tiver conteúdo no arquivo
-        printf("%s", linha); //printará as strings (conteúdo do arquivo)
+void ler_arq(char *nomearq, int tamlinha){              //recebe o nome do arquivo e o tamanho da linha
+    char linha[tamlinha];                               //o tamanho da linha é definida 
+    FILE * arq = fopen(nomearq, "r");                   //topo arquivo tem um ponteiro que vai ser aberto em modo leitura
+    while(fgets(linha, sizeof(linha), arq) != NULL){    //enquanto tiver conteúdo no arquivo
+        printf("%s", linha);                            //printará as strings (conteúdo do arquivo)
     }
-    fclose(arq); //fecha o arquivo
+    fclose(arq);                                        //fecha o arquivo
 }
 
 //função que lê arquivos com cor na letra
 void ler_arq_cor(char *nomearq, int tamlinha, char *cor){   //recebe o nome do arquivo, e a cor
-    char linha[100];  //linha tem até 100 caracteres
-    FILE * arq = fopen(nomearq, "r");  //topo arquivo tem um ponteiro que vai ser aberto em modo leitura
-    while(fgets(linha, sizeof(linha), arq) != NULL){ //enquanto tiver conteúdo no arquivo
-        printf("%s%s%s",cor, linha, RESET); //printará as strings (conteúdo do arquivo)
+    char linha[tamlinha];                                   //linha vai ter o tamanho de tamlinha
+    FILE * arq = fopen(nomearq, "r");                       //arquivo que vai ser aberto em modo leitura
+    while(fgets(linha, sizeof(linha), arq) != NULL){        //enquanto tiver conteúdo no arquivo
+        printf("%s%s%s",cor, linha, RESET);                 //printará as strings com a cor (conteúdo do arquivo)
     }
-    fclose(arq); //fecha o arquivo
+    fclose(arq);                                            //fecha o arquivo
 }
 
 //função que lê arquivos e substitui o símbolo pelo nome do símbolo ou nome definido
-void substituir(char *nomearq, int tamlinha, char *caractere, char *substitur){   //recebe o nome do arquivo
-    char linha[tamlinha];  //linha tem até 100 caracteres
-    char *separador; //ponteiro para separa o txt em sub strings
-    char *substituto; substituto = caractere; 
-    FILE * arq = fopen(nomearq, "r");  //topo arquivo tem um ponteiro que vai ser aberto em modo leitura
+void substituir(char *nomearq, int tamlinha, char *caractere, char *substituir){   //recebe o nome do arquivo, tamanho da linha, string a ser substituída, string substituta
+    char linha[tamlinha];                                                          //linha vai ter o tamanho de tamlinha 
+    char *separador;                                                               //ponteiro para separa o txt em sub strings
+    char *substituto; substituto = caractere;                                      //ponteiro substituto que vai receber os caracteres
+    FILE * arq = fopen(nomearq, "r");                                              //arquivo que vai ser aberto em modo leitura
 
-    while(fgets(linha, sizeof(linha), arq) != NULL){ //enquanto tiver conteúdo no arquivo
+    while(fgets(linha, sizeof(linha), arq) != NULL){                               //enquanto tiver conteúdo no arquivo
         
-        separador = strtok(linha, " "); // as sub strins serão separadas pelo caractere espaço
-        while(separador != NULL){ //enquanto for diferente de nulo
-            if (strstr(separador, substituto) != 0){  //aqui mudará pelo nome doi personagem
-                separador = substitur;
+        separador = strtok(linha, " ");                                            //as sub strins serão separadas pelo caractere espaço
+        while(separador != NULL){                                                  //enquanto for diferente de nulo
+            if (strstr(separador, substituto) != 0){                               //aqui ocorrerá a substituição se o caractere for igual a substring da linha
+                separador = substituir;
             }
-            printf(" %s", separador); //cada sub string do arquivo será exibido
+            printf(" %s", separador);                                              //cada sub string do arquivo será exibido
             fflush(stdout);
             separador = strtok(NULL, " ");
         }
     }
-    fclose(arq); //fecha o arquivo
+    fclose(arq);                                                                   //fecha o arquivo
 }
 
 
